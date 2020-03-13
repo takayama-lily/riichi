a82b04638ff8babee6548a2a8a2c5f3672bddd55
takayama <takayama@foxmail.com>
2020-03-13 17:54:30 +0900

    backup

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 'use strict'
-const MJ = module.exports = require('./lib/mj')
-
-MJ.calc = data=>{
-    return new MJ(data).calc()
-}
-
-MJ.isAgari = require('./lib/agari')
-MJ.syanten = require('./lib/syanten')
+module.exports = require('./lib/riichi')
